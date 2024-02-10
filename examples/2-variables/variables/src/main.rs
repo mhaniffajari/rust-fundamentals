7394f08a7b8ada88946a6a4349860f2b660ee43b
Muhammad Hanif Fajari <105597377+mhaniffajari@users.noreply.github.com>
2024-02-10 15:45:44 +0000

    Exercise_1

--- examples/2-variables/variables/src/main.rs
+++ examples/2-variables/variables/src/main.rs
@@ -1,11 +1,11 @@
 
 
 fn main() {
-    let message = "Name: Alfredo, Weight: ";
-    let weight = 190.0;
-
-    let kilos = weight / 2.2;
-    println!("{}{}", message, weight);
-    println!("{}{}", message, kilos);
+    let mut message = String :: from("Name: Muhammad Hanif, Weight: ,Kilos:");
+    message.clear();
+    let weight = 170.0;
+    let mut height = 169;
+    let kilos= weight / 2.2;
+    println!("{}{}{}", message, weight,kilos);
     
 }
---
+++ exercise/1-variables/variables/src/main.rs
@@ -0,0 +1,9 @@
+
+
+fn main() {
+
+    let message = "Name : Muhammad Hanif Fajari";
+    let weight = 154;
+    let kilos = weight/2.2;
+    println!("{} and {} kg", message, kilos);   
+}
