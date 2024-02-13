8dc164b09b30f9a46e2781c8d96da48569d877d0
Muhammad Hanif Fajari <105597377+mhaniffajari@users.noreply.github.com>
2024-02-13 09:50:11 +0000

    exercise4

---
+++ exercise/for-the-loop/src/main.rs
@@ -0,0 +1,12 @@
+fn main(){
+    // for i in 1..10{
+    //     println!("i = {}",i);
+    // }
+    // for i  in (1..=5).rev(){
+    //     println!("i = {}",i);
+    // }
+    let numbers = vec![1,2,3,4,5,6,7,8,9];
+    for n in numbers{
+        println!("n = {}",n);
+    }
+}
