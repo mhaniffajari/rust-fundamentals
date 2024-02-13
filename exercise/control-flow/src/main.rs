e2027aa6971fdb9615a714a888aeb9a4d6a1a5a2
Muhammad Hanif Fajari <105597377+mhaniffajari@users.noreply.github.com>
2024-02-13 10:12:12 +0000

    rust

--- exercise/control-flow/src/main.rs
+++ exercise/control-flow/src/main.rs
@@ -1,6 +1,11 @@
+use std::io;
+
 fn main(){
-    let name  = "Hai";
-    match name {
+    println!("Enter a word please");
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).expect("Failed to hear you");
+    //let name  = "Hai";
+    match name.trim() {
         "Good Bye" => println!("You can't leave"),
         "Hai" => println!("Welcome to the party"),
         _ => println!("What are you doing?")
